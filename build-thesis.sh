476216cd4203278be08a5d79da7f47350ef9cf93
Vineet John <iamvineetjohn@gmail.com>
2018-04-11 19:39:55 -0400

    Add introduction to problem with image of vision style transfer

--- build-thesis.sh
+++ build-thesis.sh
@@ -2,7 +2,14 @@
 
 LOGFILE="build/build.log"
 
-pdflatex -shell-escape -output-directory=build/ uw-ethesis &> $LOGFILE && \
+pdflatex -halt-on-error -shell-escape -output-directory=build/ uw-ethesis &> $LOGFILE && \
 bibtex build/uw-ethesis &>> $LOGFILE && \
-pdflatex -shell-escape -output-directory=build/ uw-ethesis  &>> $LOGFILE && \
-pdflatex -shell-escape -output-directory=build/ uw-ethesis  &>> $LOGFILE
+makeglossaries build/uw-ethesis &>> $LOGFILE && \
+pdflatex -halt-on-error -shell-escape -output-directory=build/ uw-ethesis  &>> $LOGFILE && \
+pdflatex -halt-on-error -shell-escape -output-directory=build/ uw-ethesis  &>> $LOGFILE
+
+if [ $? -eq 0 ]; then
+    echo "Build completed successfully"
+else
+    echo "Build failed"
+fi
